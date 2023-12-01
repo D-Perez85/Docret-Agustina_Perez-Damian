be6f9eb97462fb627e59cf2396b5e5d2ecdc8872
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2023-11-30 21:59:47 -0300

    Correccion en Proyecto

--- clinica-odontologica/src/main/java/com/backend/clinicaodontologica/repository/DomicilioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.backend.clinicaodontologica.repository;
-
-import com.backend.clinicaodontologica.entity.Domicilio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DomicilioRepository extends JpaRepository <Domicilio, Long> {
-
-    
-}
